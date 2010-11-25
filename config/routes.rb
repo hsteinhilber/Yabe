cda427bc9522ab7f8bbb59629b1b74a5345eab10
Harry Steinhilber, Jr <harry.steinhilber@kohls.com>
2010-11-24 23:36:28 -0500

    new more sussinct routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Yabe::Application.routes.draw do
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+
+  root :to => 'pages#home'
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"
