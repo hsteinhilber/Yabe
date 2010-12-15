21983622c9e2ab8ca04c178e9c5455a7492a9c12
Harry Steinhilber, Jr <harry.steinhilber@kohls.com>
2010-12-15 15:48:36 -0500

    removed extraneous model annotation comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,20 +11,6 @@
 #  encrypted_password :string(255)
 #  salt               :string(255)
 #
-
-# == Schema Information
-# Schema version: 20101203230348
-#
-# Table name: users
-#
-#  id                 :integer         not null, primary key
-#  name               :string(255)
-#  email              :string(255)
-#  created_at         :datetime
-#  updated_at         :datetime
-#  encrypted_password :string(255)
-#  salt               :string(255)
-#
 require 'digest'
 
 class User < ActiveRecord::Base
