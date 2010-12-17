55ae9c96f48a0c68220fd8477484e3c783f487e3
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2010-12-16 20:03:21 -0500

    initial session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Yabe::Application.routes.draw do
   resources :posts
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
