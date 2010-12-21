54b4f807afbf8d41d10961b5e94a81bb7f696e5c
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2010-12-21 18:20:46 -0500

    added links to navigation for loggin in/out

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -16,4 +16,34 @@ describe "LayoutLinks" do
     get '/about'
     response.should have_selector('title', :content => "About")
   end
+
+  describe "when not logged in" do
+    it "should have a log in link" do
+      visit root_path
+      response.should have_selector("a", :href => login_path,
+                                         :content => "Log in")
+    end
+  end
+
+  describe "when logged in" do
+    before(:each) do
+      @user = Factory(:user)
+      visit login_path
+      fill_in :email, :with => @user.email
+      fill_in :password, :with => @user.password
+      click_button
+    end
+
+    it "should have a log out link" do
+      visit root_path
+      response.should have_selector("a", :href => logout_path,
+                                         :content => "Log out")
+    end
+
+    it "should have a profile link" do
+      visit root_path
+      response.should have_selector("a", :href => user_path(@user),
+                                         :content => "Profile")
+    end
+  end
 end
