ea68c89683dea79a581ab43acc488a363d06bd39
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2010-12-20 20:35:17 -0500

    allow users to sign in and store a session cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      flash.now[:error] = "Invalid email address or password."
+      @title = "Login"
+      render :new
+    else
+      sign_in user
+      redirect_to user
+    end
   end
 
   def destroy
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -14,4 +14,45 @@ describe SessionsController do
       response.should have_selector("title", :content => "Login")
     end
   end
+
+  describe "POST 'create'" do
+    describe "invalid login" do
+      before(:each) do
+        @attr = { :email => "user@example.com", :password => "invalid" }
+      end
+
+      it "should re-render the 'new' page" do
+        post :create, :session => @attr
+        response.should render_template(:new)
+      end
+
+      it "should have the right title" do
+        post :create, :session => @attr
+        response.should have_selector("title", :content => "Login")
+      end
+
+      it "should have an error message" do
+        post :create, :session => @attr
+        flash.now[:error].should =~ /invalid/i
+      end
+    end
+
+    describe "with valid email and password" do
+      before(:each) do
+        @user = Factory(:user)
+        @attr = { :email => @user.email, :password => @user.password }
+      end
+
+      it "should sign the user in" do
+        post :create, :session => @attr
+        controller.current_user.should == @user
+        controller.should be_logged_in
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :session => @attr
+        response.should redirect_to(user_path(@user))
+      end
+    end
+  end
 end
