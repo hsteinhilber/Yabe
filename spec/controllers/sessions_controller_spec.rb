76a30f05e18bddbcfbbc2c39b1c1385ac08a17c4
Harry Steinhilber, Jr <harry.steinhilber@gmail.com>
2010-12-20 20:44:21 -0500

    users can now log out

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -55,4 +55,14 @@ describe SessionsController do
       end
     end
   end
+
+  describe "DELETE 'destroy'" do
+
+    it "should log a user out" do
+      test_sign_in(Factory(:user))
+      delete :destroy
+      controller.should_not be_logged_in
+      response.should redirect_to(root_path)
+    end
+  end
 end
