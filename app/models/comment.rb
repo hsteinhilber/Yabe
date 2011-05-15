80d719b0caa19cb02e3a8968f7bf2f042614d245
Harry Steinhilber <harry.steinhilber@gmail.com>
2011-05-15 10:29:43 -0400

    comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,9 @@ class Comment < ActiveRecord::Base
   belongs_to :post
 
   default_scope :order => "created_at ASC"
+  validates :post_id, :presence => true
+  validates :name, :presence => true, :length => { :maximum => 75 }
+  validates :email, :presence => true, :format => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :url, :format => /\A((\s*)|(http(s?)\:\/\/[0-9a-z]([-.\w]*[0-9a-z])*(:(0-9)*)*(\/?)([a-z0-9\-\.\?\,\'\/\\\+&%\$#_=]*)))\z/i 
+  validates :body, :presence => true, :length => { :maximum => 255 }
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -31,4 +31,67 @@ describe Comment do
       @comment.post.should == @post
     end
   end
+
+  describe "validation" do
+
+    it "should require a post" do
+      Comment.new(@attr).should_not be_valid
+    end
+
+    it "should require a name" do
+      @post.comments.build(@attr.merge(:name => " ")).should_not be_valid
+    end
+
+    it "should not allow names to be too long" do
+      long_name = "a" * 76
+      @post.comments.build(@attr.merge(:name => long_name)).should_not be_valid
+    end
+
+    it "should require an email address" do
+      @post.comments.build(@attr.merge(:email => "")).should_not be_valid
+    end
+
+    addresses = %w[commenter@example.com ALLCAP_COMMENTER@foo.bar.com basic.commenter@mycompany.us]
+    addresses.each do |address|
+      it "should accept valid email addresses like '#{address}'" do
+        @post.comments.build(@attr.merge(:email => address)).should be_valid
+      end
+    end
+
+    addresses = %w[commenter@example commenter_example.com something@com another@blah.]
+    addresses.each do |address|
+      it "should reject invalid email addresses like '#{address}'" do
+        @post.comments.build(@attr.merge(:email => address)).should_not be_valid
+      end
+    end
+
+    it "should allow a blank url" do
+      @post.comments.build(@attr.merge(:url => "  ")).should be_valid
+    end
+
+    urls = %w[http://www.example.com http://example.com https://www.example.com http://blog.example.com http://my.blog.example.com 
+              http://example.com/blog http://www.example.com/mysite/blog/ http://example.com/index.html http://example.com/index?q=hello%20world
+              http://example.com/posts/5#comments]
+    urls.each do |url|
+      it "should accept valid urls like '#{url}'" do
+        @post.comments.build(@attr.merge(:url => url)).should be_valid
+      end
+    end
+
+    urls = %w[www.example.com http://www@example.com file://www.example.com]
+    urls.each do |url|
+      it "should not accept invalid urls like '#{url}'" do
+        @post.comments.build(@attr.merge(:url => url)).should_not be_valid
+      end
+    end
+
+    it "should require a body" do
+      @post.comments.build(@attr.merge(:body => " ")).should_not be_valid
+    end
+
+    it "should not allow the body to be too long" do
+      long_body = "a" * 256
+      @post.comments.build(@attr.merge(:body => long_body)).should_not be_valid
+    end
+  end
 end
