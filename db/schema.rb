3adb90e30a7eb22f2db4f6e51c4bce355c55f10c
Harry Steinhilber <harry.steinhilber@gmail.com>
2011-05-14 20:23:35 -0400

    initial comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110514110539) do
+ActiveRecord::Schema.define(:version => 20110515001604) do
 
   create_table "authors", :force => true do |t|
     t.string   "name",               :limit => 75
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(:version => 20110514110539) do
   end
 
   add_index "authors", ["email"], :name => "index_users_on_email", :unique => true
+
+  create_table "comments", :force => true do |t|
+    t.integer  "post_id"
+    t.string   "name"
+    t.string   "url"
+    t.string   "email"
+    t.string   "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
 
   create_table "posts", :force => true do |t|
     t.string   "title",      :limit => 35
